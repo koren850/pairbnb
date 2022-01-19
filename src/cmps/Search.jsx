d5e03367a131cbeae49283f4e8f1cd2a33480151
Michael Aharoni <michaelaharoni001@gmail.com>
2022-01-19 17:48:20 +0200

    search btn hover

--- src/cmps/Search.jsx
+++ src/cmps/Search.jsx
@@ -1,9 +1,13 @@
 import searchSvg from '../styles/svg/search.svg';
+import { SpecialButton } from './SpacialButton';
 
 export function Search() {
 
     return (<button className="search">
         <p>Start your search</p>
-<img src={searchSvg} className="search-svg" alt=""/>
+        <img src={searchSvg} className="search-svg" alt=""/>
+        {/* <div className='spacial-btn'>
+        <SpecialButton text={<img src={searchSvg} className="search-svg" alt=""/>}/>
+        </div> */}
     </button>)
 }
