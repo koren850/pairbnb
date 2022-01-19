dffa756b79facb387269a91497b14fe07a5ac3f6
koren850 <korenah3@gmail.com>
2022-01-19 17:47:31 +0200

    header btn

--- src/cmps/Search.jsx
+++ src/cmps/Search.jsx
@@ -1,9 +1,12 @@
-import searchSvg from '../styles/svg/search.svg';
-
+import searchSvg from "../styles/svg/search.svg";
+import { SpecialButton } from "./SpacialButton";
 export function Search() {
-
-    return (<button className="search">
-        <p>Start your search</p>
-<img src={searchSvg} className="search-svg" alt=""/>
-    </button>)
+	return (
+		<button className='search'>
+			<p>Start your search</p>
+			<div className='spacial-btn search-spacial-btn'>
+				<SpecialButton text={<img src={searchSvg} className='search-svg' alt='' />} />
+			</div>
+		</button>
+	);
 }
