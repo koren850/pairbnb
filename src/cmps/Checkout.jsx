782852e8e3126fda5bc0b6d8582d54083228f128
koren850 <korenah3@gmail.com>
2022-01-19 15:44:57 +0200

    progress

---
+++ src/cmps/Checkout.jsx
@@ -0,0 +1,143 @@
+export function Checkout() {
+	return (
+		<main className='checkout-btn'>
+			<section class='order-container'>
+				<div class='order-form-header'>
+					<p>
+						<span class='cost'>$150</span> / night
+					</p>
+					<p>
+						4.38 <span class='reviews'>(4 reviews)</span>
+					</p>
+				</div>
+
+				<div class='order-data'>
+					<div class='date-picker'>
+						<div class='date-input'>
+							<label>check in</label>
+							<input placeholder='Tue Sep 07 2021'></input>
+						</div>
+						<div class='date-input'>
+							<label>check out</label>
+							<input placeholder='Tue Sep 07 2021'></input>
+						</div>
+					</div>
+
+					<div class='guest-input'>
+						<label>guests</label>
+						<input placeholder='2'></input>
+					</div>
+				</div>
+
+				<div class='btn-container'>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='cell'></div>
+					<div class='content'>
+						<button class='action-btn'>
+							<span>Check availability</span>
+						</button>
+					</div>
+				</div>
+			</section>
+			<p class='footer'>Report this listing</p>
+		</main>
+	);
+}
