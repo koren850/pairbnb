900e0dbb9552569673d9441c3f162f9a13188f8e
koren850 <korenah3@gmail.com>
2022-01-19 10:49:43 +0200

    progress

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,41 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { loadStays } from '../store/stay.action.js';
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { loadStays } from "../store/stay.action.js";
 import { Filter } from "../cmps/Filter";
-
 
 // import { Link } from "react-router-dom";
 
 import { StayList } from "../cmps/StayList.jsx";
 
-export function _Explore({ }) {
-
-	const [stays, setStays] = useState(null)
+export function _Explore({}) {
+	const [stays, setStays] = useState(null);
 
 	useEffect(() => {
-		const stay = loadStays()
-	})
-
+		const stay = loadStays();
+	});
 
 	return (
 		// const { stays } = t
-		< main >
+		<main>
 			<div>Explore page</div>
 			<Filter />
 
 			<StayList />
-		</main >
-	)
-
+		</main>
+	);
 }
 
 function mapStateToProps({ stayModule }) {
 	return {
 		stays: stayModule.toys,
-	}
+	};
 }
 const mapDispatchToProps = {
 	loadStays,
 };
 
-export const Explore = connect(mapStateToProps, mapDispatchToProps)(_Explore)
+export const Explore = connect(mapStateToProps, mapDispatchToProps)(_Explore);
