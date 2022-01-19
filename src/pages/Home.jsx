4dda6e17fa7dc02e54a9b6fecfd87431be2cf18f
Michael Aharoni <michaelaharoni001@gmail.com>
2022-01-19 17:56:08 +0200

    detail layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import heroImg from '../styles/img/home-page-hero.jpg'
 
 export function Home() {
-	return <main>Home</main>;
+	return <main className="home main-layout">
+		<img className="middle-layout" src={heroImg}/>
+	</main>;
 }
