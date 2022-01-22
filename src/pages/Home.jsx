a4b9e1a3be9b2ec74e2bca7214e599b9579ed4fb
Michael Aharoni <michaelaharoni001@gmail.com>
2022-01-22 09:55:05 +0200

    six day

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,8 @@ export function Home() {
 	return (
 		<main className='home main-layout'>
 			<section className='hero full-layout'>
-				<img src='https://res.cloudinary.com/dqj9g5gso/image/upload/v1642610299/imgs/HD_wallpaper__brown_wooden_dock_and_cottages_Maldives_resort_artificial_lights_zxkna8.jpg' />
+				<img 
+				src='https://res.cloudinary.com/dqj9g5gso/image/upload/v1642610299/imgs/HD_wallpaper__brown_wooden_dock_and_cottages_Maldives_resort_artificial_lights_zxkna8.jpg' />
 				<h1 className='hero-title'>Not sure where to go? Perfect.</h1>
 				<Link to={"/explore"}>
 					<button className='hero-btn home-btn'>
