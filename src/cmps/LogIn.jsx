971641d90929ddbffee090840e3b5578af904b33
koren850 <korenah3@gmail.com>
2022-01-22 10:54:12 +0200

    day 5 first push

--- src/cmps/LogIn.jsx
+++ src/cmps/LogIn.jsx
@@ -1,162 +1,146 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import { Link } from 'react-router-dom';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useHistory } from 'react-router-dom';
-import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useHistory } from "react-router-dom";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 import { FacebookLoginButton } from "react-social-login-buttons";
 import { GoogleLoginButton } from "react-social-login-buttons";
-import { GoogleLogin } from 'react-google-login';
-
+import { GoogleLogin } from "react-google-login";
 
 const theme = createTheme();
 
 export function LogIn({ setIsSubmitting, signingIn }) {
-    const history = useHistory();
+	const history = useHistory();
 
-    const responseFacebook = (response) => {
-        const credentials = {
-            fullName: response.name,
-            email: response.email,
-            imgSrc: response.picture.data.url
-        }
-        console.log(credentials, "וואלה מגניב רצח הא?");
-        setIsSubmitting(true);
-        setTimeout(() => {
-            signingIn(credentials);
-            history.push('/');
-        }, 2000)
-    }
+	const responseFacebook = (response) => {
+		const credentials = {
+			fullName: response.name,
+			email: response.email,
+			imgSrc: response.picture.data.url,
+		};
+		console.log(credentials, "וואלה מגניב רצח הא?");
+		setIsSubmitting(true);
+		setTimeout(() => {
+			signingIn(credentials);
+			history.push("/");
+		}, 2000);
+	};
 
-    const responseGoogle = (response) => {
-        const credentials = {
-            fullName: response.profileObj.name,
-            email: response.profileObj.email,
-            imgSrc: response.profileObj.imageUrl
-        }
-        console.log(credentials, "וואלה מגניב רצח הא?");
-        setIsSubmitting(true);
-        setTimeout(() => {
-            signingIn(credentials);
-            history.push('/');
-        }, 2000)
-    }
+	const responseGoogle = (response) => {
+		const credentials = {
+			fullName: response.profileObj.name,
+			email: response.profileObj.email,
+			imgSrc: response.profileObj.imageUrl,
+		};
+		console.log(credentials, "וואלה מגניב רצח הא?");
+		setIsSubmitting(true);
+		setTimeout(() => {
+			signingIn(credentials);
+			history.push("/");
+		}, 2000);
+	};
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        const credentials = {
-            username: data.get('username'),
-            password: data.get('password'),
-        }
-        setIsSubmitting(true);
-        setTimeout(() => {
-            signingIn(credentials);
-            history.push('/');
-        }, 2000)
-    };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		const credentials = {
+			username: data.get("username"),
+			password: data.get("password"),
+		};
+		setIsSubmitting(true);
+		setTimeout(() => {
+			signingIn(credentials);
+			history.push("/");
+		}, 2000);
+	};
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="username"
-                            label="User Name"
-                            type="username"
-                            id="username"
-                            autoComplete="current-username"
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        {/* <FormControlLabel
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component='main' maxWidth='xs'>
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}>
+					<Typography component='h1' variant='h5'>
+						Sign in
+					</Typography>
+					<Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+						<TextField margin='normal' required fullWidth name='email' label='E-mail' type='email' id='email' autoComplete='current-email' />
+						<TextField margin='normal' required fullWidth name='password' label='Password' type='password' id='password' autoComplete='current-password' />
+						{/* <FormControlLabel
                             control={<Checkbox
                                 style={{ color: 'green' }}
                                 value="remember" color="primary" />}
                             label="Remember me"
                         /> */}
 
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            style={{ backgroundColor: '#FF385C' }}
-                            // sx={{ mt: 3, mb: 2 }}
-                            className="wow"
-                        >
-                            Sign In
-                        </Button>
+						<Button
+							type='submit'
+							fullWidth
+							variant='contained'
+							style={{ backgroundColor: "#FF385C" }}
+							// sx={{ mt: 3, mb: 2 }}
+							className='wow'>
+							Sign In
+						</Button>
 
-                        <GoogleLogin
-                            scope="profile email"
-                            redirect_uri="https://localhost:3000/user/login"
-                            clientId="490177482976-lt2ap2i9ei79difra3envh2b1g1528bn.apps.googleusercontent.com"
-                            buttonText="Login with Google acccount"
-                            onSuccess={responseGoogle}
-                            onFailure={responseGoogle}
-                            render={renderProps => (
-                                <GoogleLoginButton style={{ display: 'flex', justifyContent: 'center', height: '35px', margin: '10px 0', width: '396px' }} onClick={renderProps.onClick} disabled={renderProps.disabled}>Sign in with Google</GoogleLoginButton>
-                            )}
-                            cookiePolicy={'single_host_origin'}
-                        />
+						<GoogleLogin
+							scope='profile email'
+							redirect_uri='https://localhost:3000/user/login'
+							clientId='490177482976-lt2ap2i9ei79difra3envh2b1g1528bn.apps.googleusercontent.com'
+							buttonText='Login with Google acccount'
+							onSuccess={responseGoogle}
+							onFailure={responseGoogle}
+							render={(renderProps) => (
+								<GoogleLoginButton
+									style={{ display: "flex", justifyContent: "center", height: "35px", margin: "10px 0", width: "396px" }}
+									onClick={renderProps.onClick}
+									disabled={renderProps.disabled}>
+									Sign in with Google
+								</GoogleLoginButton>
+							)}
+							cookiePolicy={"single_host_origin"}
+						/>
 
-                        <FacebookLogin
-                            appId="960690547913550"
-                            fields="name,email,picture"
-                            render={renderProps => (
-                                <FacebookLoginButton style={{ display: 'flex', justifyContent: 'center', height: '35px', margin: '10px 0', width: '396px' }} onClick={renderProps.onClick} disabled={renderProps.disabled}>Sign in with Facebook</FacebookLoginButton>
-                            )}
-                            callback={responseFacebook}
-                        />
-                        <Grid container>
-                            <Grid item xs>
-                                <Link to="/signup">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link to="/user/signup">
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-            </Container>
-        </ThemeProvider>
-    );
+						<FacebookLogin
+							appId='960690547913550'
+							fields='name,email,picture'
+							render={(renderProps) => (
+								<FacebookLoginButton
+									style={{ display: "flex", justifyContent: "center", height: "35px", margin: "10px 0", width: "396px" }}
+									onClick={renderProps.onClick}
+									disabled={renderProps.disabled}>
+									Sign in with Facebook
+								</FacebookLoginButton>
+							)}
+							callback={responseFacebook}
+						/>
+						<Grid container>
+							<Grid item xs>
+								<Link to='/signup'>Forgot password?</Link>
+							</Grid>
+							<Grid item>
+								<Link to='/user/signup'>{"Don't have an account? Sign Up"}</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+			</Container>
+		</ThemeProvider>
+	);
 }
-
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -27,18 +27,21 @@ function getUsers() {
 
 async function getById(userId) {
     // const user = await storageService.get('user', userId)
-    const user = await httpService.get(`user/${userId}`)
-    gWatchedUser = user;
-    return user;
+    //  const user = await httpService.get(`user/${userId}`) ******* When backend is up uncomment
+    // gWatchedUser = user;  ******* When backend is up uncomment
+    // return user;  ******* When backend is up uncomment
+    gWatchedUser = userId;
+    return userId;
 }
 function remove(userId) {
     // return storageService.remove('user', userId)
-    return httpService.delete(`user/${userId}`)
+    // return httpService.delete(`user/${userId}`) ******* When backend is up uncomment
+    return userId
 }
 
 async function update(user) {
     // await storageService.put('user', user)
-    user = await httpService.put(`user/${user._id}`, user)
+    // user = await httpService.put(`user/${user._id}`, user)******* When backend is up uncomment
     // Handle case in which admin updates other user's details
     if (getLoggedinUser()._id === user._id) _saveLocalUser(user)
     return user;
@@ -49,21 +52,23 @@ async function login(userCred) {
     // const user = users.find(user => user.username === userCred.username)
     // return _saveLocalUser(user)
 
-    const user = await httpService.post('auth/login', userCred)
+    // const user = await httpService.post('auth/login', userCred) ******* When backend is up uncomment
     // socketService.emit('set-user-socket', user._id);
-    if (user) return _saveLocalUser(user)
+    //if (user) return _saveLocalUser(user) ******* When backend is up uncomment
+    if (userCred) return _saveLocalUser(userCred)
 }
 async function signup(userCred) {
     // userCred.score = 10000;
     // const user = await storageService.post('user', userCred)
-    const user = await httpService.post('auth/signup', userCred)
+    // const user = await httpService.post('auth/signup', userCred) ******* When backend is up uncomment
     // socketService.emit('set-user-socket', user._id);
-    return _saveLocalUser(user)
+    // return _saveLocalUser(user)******* When backend is up uncomment
+    return _saveLocalUser(userCred)
 }
 async function logout() {
     sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
     // socketService.emit('unset-user-socket');
-    return await httpService.post('auth/logout')
+    // return await httpService.post('auth/logout')******* When backend is up uncomment
 }
 
 async function changeScore(by) {
