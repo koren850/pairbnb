1ea62fbc4e3ddcfa2b5df83c3d18c58d5e6fc3ea
koren850 <korenah3@gmail.com>
2022-01-26 13:27:43 +0200

    css fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,15 +15,17 @@ export function Home() {
 	const heroUrl = "https://res.cloudinary.com/dqj9g5gso/image/upload/v1642610299/imgs/HD_wallpaper__brown_wooden_dock_and_cottages_Maldives_resort_artificial_lights_zxkna8.jpg";
 
 	useEffect(() => {
-		dispatch(setParams({
-			adults: 1,
-			checkIn: null,
-			checkOut: null,
-			children: 0,
-			guestsCount: 1,
-			infants: 0,
-			location: ""
-		}))
+		dispatch(
+			setParams({
+				adults: 1,
+				checkIn: null,
+				checkOut: null,
+				children: 0,
+				guestsCount: 1,
+				infants: 0,
+				location: "",
+			})
+		);
 		dispatch(toggleIsExplore(false));
 		dispatch(toggleHeaderIsActive(true));
 		dispatch(toggleHeaderIsTop(true));
@@ -39,30 +41,38 @@ export function Home() {
 			<section className='hero full-layout'>
 				<img src={heroUrl} alt='Hero Img' />
 				<h1 className='hero-title'>Not sure where to go? Perfect.</h1>
-				<Link to={"/explore"}>
+				<Link className='home-btn' to={"/explore"}>
 					<button className='hero-btn home-btn'>
 						<span>Explore</span>
 					</button>
 				</Link>
-				</section>
+			</section>
 			<h1 className='middle-layout cities-title'>Popular cities around the word.</h1>
 			<section className='popular-cities middle-layout'>
-			<Link to={`/explore/location=paris`}><article className='city-container'>
-					<img src={parisImg} alt='City Img' />
-					<h1>Paris</h1>
-				</article></Link>
-				<Link to={`/explore/location=jerusalem`}><article className='city-container'>
-					<img src={jerusalemImg} alt='City Img' />
-					<h1>Jerusalem</h1>
-				</article></Link>
-				<Link to={`/explore/location=dubai`}><article className='city-container'>
-					<img src={dubaiImg} alt='City Img' />
-					<h1>Dubai</h1>
-				</article></Link>
-				<Link to={`/explore/location=las-vegas`}><article className='city-container'>
-					<img src={vegasImg} alt='City Img' />
-					<h1>Las Vegas</h1>
-				</article></Link>
+				<Link to={`/explore/location=paris`}>
+					<article className='city-container'>
+						<img src={parisImg} alt='City Img' />
+						<h1>Paris</h1>
+					</article>
+				</Link>
+				<Link to={`/explore/location=jerusalem`}>
+					<article className='city-container'>
+						<img src={jerusalemImg} alt='City Img' />
+						<h1>Jerusalem</h1>
+					</article>
+				</Link>
+				<Link to={`/explore/location=dubai`}>
+					<article className='city-container'>
+						<img src={dubaiImg} alt='City Img' />
+						<h1>Dubai</h1>
+					</article>
+				</Link>
+				<Link to={`/explore/location=las-vegas`}>
+					<article className='city-container'>
+						<img src={vegasImg} alt='City Img' />
+						<h1>Las Vegas</h1>
+					</article>
+				</Link>
 			</section>
 			<section className='host middle-layout'>
 				<img src={hostImg} alt='Host Img' />
